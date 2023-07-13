4aa2809f5eca74188145fab0b23b2d3afc2d438c
johannlacroix <133893054+johannlacroix@users.noreply.github.com>
2023-07-13 17:55:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿const openButtons = document.querySelectorAll('.open-button');
+const closeButtons = document.querySelectorAll('.close-button');
+const sections = document.querySelectorAll('section');
+
+// Ajoute un événement de clic sur chaque bouton "Open"
+openButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const target = button.getAttribute('href');
+    document.querySelector(target).classList.add('active');
+  });
+});
+
+// Ajoute un événement de clic sur chaque bouton "Close"
+closeButtons.forEach((button) => {
+
